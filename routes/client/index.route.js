d42234630478750dc4821ea29b06f16e5111a7f8
unknown <ndkhanh2101362@student.edu.vn>
2024-03-09 23:07:12 +0700

    Chia nhỏ route trong file index.route thành các route nhỏ hơn

--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,9 +1,8 @@
+const productRoutes = require("./product.route");
+const homeRoutes = require("./home.route");
+
 module.exports = (app) =>{
-    app.get('/', (req, res) => {
-        res.render("client/pages/home/index");
-    });
+    app.use("/", homeRoutes);
     
-    app.get("/products", (req, res) => {
-        res.send("Trang dan danh sách sản phẩm");
-    });
+    app.use("/products", productRoutes);
 }
