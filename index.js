0cdab0158e740971c0c6c4860d1d302e4f4bf55a
unknown <ndkhanh2101362@student.edu.vn>
2024-03-09 22:34:11 +0700

    cấu hình pug và folder views

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//Cấu hình pug
+app.set("views", "./views");
+app.set("view engine", "pug");
+
 app.get('/', (req, res) => {
-    res.send("Trang chủ");
+    res.render("client/pages/home/index");
 });
 
 app.get("/products", (req, res) => {
