51465de223b563fa818eac8c2ea3c0af9dd800ee
unknown <ndkhanh2101362@student.edu.vn>
2024-03-10 21:21:44 +0700

    Lấy dữ liệu in ra giao diện

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,3 +1,18 @@
-module.exports.products = (req, res) => {
-    res.render("client/pages/products/index");
+const Product = require("../../models/product.model");
+
+module.exports.products = async (req, res) => {
+    const products = await Product.find({
+        status : "active",
+        deleted : false
+    });
+    
+    const newProducts = products.map((item) => {
+        item.priceNew = (item.price * (100 - item.discountPercentage)/100).toFixed(0);
+        return item;
+    })
+
+    res.render("client/pages/products/index",{
+        pageTitle : "Danh sách sản phẩm",
+        products : newProducts
+    });
 }
