69d35715a4c53c343b21e3b78babd358d70465e6
unknown <ndkhanh2101362@student.edu.vn>
2024-03-30 20:09:07 +0700

    Tạo chức năng lọc theo trạng thái

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,9 +1,15 @@
 const Product = require("../../models/product.model");
 
 module.exports.index = async (req, res) => {
-    const products = await Product.find({
+    let find = {
         deleted : false
-    });
+    }
+
+    if(req.query.status){
+        find.status = req.query.status;
+    }
+
+    const products = await Product.find(find);
 
     res.render("admin/pages/products/index", {
         pageTitle : "Trang sản phẩm",
