33a563b53adfaabbaa3ddd1fcbbc66347bf64f05
unknown <ndkhanh2101362@student.edu.vn>
2024-03-30 07:40:26 +0700

    Lấy sản phẩm in ra giao diện bên admin

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,5 +1,12 @@
-module.exports.index = (req, res) => {
+const Product = require("../../models/product.model");
+
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        deleted : false
+    });
+
     res.render("admin/pages/products/index", {
-        pageTitle : "Trang sản phẩm"
+        pageTitle : "Trang sản phẩm",
+        products : products
     });
 }
